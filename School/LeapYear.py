93ce613b9c05fa19b484f5470554f86385e80d96
Ethan <81988057+EthanCantCode@users.noreply.github.com>
2026-04-06 19:49:10 -0500

    Added a description about the code
    Added a comment to describe the purpose of the script.

--- School/LeapYear.py
+++ School/LeapYear.py
@@ -1,3 +1,5 @@
+#This script checks to see if a year you inputed is a leap year or not
+
 def leap(year):
     if year % 400 == 0 or year % 4 == 0 and year % 100 != 0:
         print(year, "is a leap year")
